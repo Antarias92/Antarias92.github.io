11895919c24ffe81efb8770d63b9d413aec59ee3
Anthony Arias <aarias92@gmail.com>
2025-02-25 16:39:30 -0600

    Created function to gather api info

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,17 @@
 const api_key = "898c520d92754382ae13cdc6b1f8749d";
 const api_URL = "https://newsapi.org/v2/everything?q=";
+
+const news_container = document.getElementById("news_container");
+
+async function renderRandomNews(){
+    try{
+        const headlineURL = `https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=12&apiKey=${api_key}`;
+        const response = await fetch(headlineURL);
+        const info = await response.json();
+        console.log(info);
+    }
+    catch(errror){
+        console.error("Error rendering news", error);
+        return [];
+    }
+}
