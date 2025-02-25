9b65aab4d07a67fda401a3ff91f5ea49f6f49aa4
Anthony Arias <aarias92@gmail.com>
2025-02-25 17:36:50 -0600

    Need to fix displayNews function

--- JS/main.js
+++ JS/main.js
@@ -3,15 +3,38 @@ const api_URL = "https://newsapi.org/v2/everything?q=";
 
 const news_container = document.getElementById("news_container");
 
-async function renderRandomNews(){
+async function renderHeadlines(){
     try{
         const headlineURL = `https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=12&apiKey=${api_key}`;
         const response = await fetch(headlineURL);
         const info = await response.json();
+        return info.articles;
         console.log(info);
     }
-    catch(errror){
+    catch(error){
         console.error("Error rendering news", error);
         return [];
     }
 }
+
+function displayNews(articles){
+    news_container.innerHTML = "";
+    articles.forEach((article) => {
+        const newsCard = document.createElement("div");
+        newsCard.classList.add("news_card");
+        const img = document.createElement("img");
+        img.src = article.urlToImage;
+
+        newsCard.appendChild(img);
+    });
+}
+
+(async () => {
+    try{
+        const articles = await renderHeadlines();
+        displayNews(articles);
+    }
+    catch(error){
+        console.error("Error rendering news", error);
+    }
+})();
