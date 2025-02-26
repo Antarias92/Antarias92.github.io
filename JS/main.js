c65c3d8d8f4e012f444cd352a18abb54c2ac8997
Anthony Arias <aarias92@gmail.com>
2025-02-25 20:28:53 -0600

    Fix commented code to get search working

--- JS/main.js
+++ JS/main.js
@@ -3,11 +3,13 @@ const api_key = "898c520d92754382ae13cdc6b1f8749d";
 const api_URL = "https://newsapi.org/v2/everything?q=";
 
 const news_container = document.getElementById("news_container");
+const search_input = document.getElementById("search_input");
+const search_button = document.getElementById("search_button");
 
 
 async function renderHeadlines(){
     try{
-        const headlineURL = `https://newsapi.org/v2/top-headlines?country=us&pageSize=13&apiKey=${api_key}`;
+        const headlineURL = `https://newsapi.org/v2/top-headlines?country=us&pageSize=1&apiKey=${api_key}`;
         const response = await fetch(headlineURL);
         const info = await response.json();
         return info.articles;
@@ -17,6 +19,25 @@ async function renderHeadlines(){
         return [];
     }
 }
+
+
+/*async function getQuery(query){
+    try{
+        const headlineURL = `https://newsapi.org/v2/everything?q=${query}&pageSize=13&apiKey=${api_key}`;
+        const response = await fetch(headlineURL);
+        const info = await response.json();
+        const newsArticles = info.articles.filter(
+            (article) => article.urlToImage
+        );
+        return newsArticles;
+    }
+    catch(error){
+        console.error("Error rendering news", error);
+        return [];
+    }
+}*/
+
+
 
 function displayNews(articles){
     news_container.innerHTML = "";
@@ -32,6 +53,22 @@ function displayNews(articles){
    
 }
 
+
+/*search_button.addEventListener("click", async () => {
+    const query = search_input.value.trim();
+    if(query !== ""){
+        try{
+            const articles = await getQuery(query);
+            displayNews(articles);
+        }
+        catch(error){
+            console.log("Search not found", error);
+        }
+    }
+});*/
+
+
+
 (async () => {
     try{
         const articles = await renderHeadlines();
@@ -41,3 +78,6 @@ function displayNews(articles){
         console.error("Error rendering news", error);
     }
 })();
+
+
+
