0c34d6b26dcba08cdff6806487681cd4dba71d6b
Anthony Arias <aarias92@gmail.com>
2025-02-27 13:56:50 -0600

    switched to GIPHY api

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,22 @@
 
-const api_key = "898c520d92754382ae13cdc6b1f8749d";
-const api_URL = "https://newsapi.org/v2/everything?q=";
+const api_key = "dMC2OFEnh9THjWRT1OjjpLm0klaKAlCW";
 
-const news_container = document.getElementById("news_container");
+
+const gif_container = document.getElementById("gif_container");
 const search_input = document.getElementById("search_Input");
 const search_button = document.getElementById("search_button");
 
 
-async function renderHeadlines(){
+async function renderHomepage(){
     try{
-        console.log("Loading top headlines...");
-        const headlineURL = `https://newsapi.org/v2/top-headlines?country=us&pageSize=13&apiKey=${api_key}`;
-        const response = await fetch(headlineURL);
+        console.log("Loading homepage...");
+        const gifURL = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=dog&limit=12`;
+        const response = await fetch(gifURL);
         const info = await response.json();
-        return info.articles;
+        return info.data;
     }
     catch(error){
-        console.error("Error rendering news", error);
+        console.error("Error rendering gifs", error);
         return [];
     }
 }
@@ -25,36 +25,36 @@ async function renderHeadlines(){
 async function getQuery(query){
     try{
         console.log("Getting your query");
-        const headlineURL = `https://newsapi.org/v2/everything?q=${query}&pageSize=13&apiKey=${api_key}`;
-        const response = await fetch(headlineURL);
+        const gifURL = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${query}&limit=8`;
+        const response = await fetch(gifURL);
         const info = await response.json();
-        const newsArticles = info.articles.filter(
-            (article) => article.urlToImage
+        const gifResults = info.data.filter(
+            (data) => data.images.original.url
         );
-        return newsArticles;
+        console.log("Here are your results");
+        return gifResults;
     }
     catch(error){
-        console.error("Error rendering news", error);
+        console.error("Error rendering gifs", error);
         return [];
     }
 }
 
 
-
-function displayNews(articles){
-    news_container.innerHTML = "";
-    articles.forEach((article) => {
-        const newsCard = document.createElement("div");
-        newsCard.classList.add("news_card");
+function displayGifs(gifResults){
+    gif_container.innerHTML = "";
+    gifResults.forEach((data) => {
+        const gifCard = document.createElement("div");
+        gifCard.classList.add("gif_card");
         const img = document.createElement("img");
-        img.src = article.urlToImage;
-        img.alt = article.title;
+        img.src = data.images.original.url;
+        img.alt = data.title;
         const title = document.createElement("h3");
-        title.textContent = article.title;
+        title.textContent = data.title;
 
-        newsCard.appendChild(img);
-        newsCard.appendChild(title);
-        news_container.appendChild(newsCard);
+        gifCard.appendChild(img);
+        gifCard.appendChild(title);
+        gif_container.appendChild(gifCard);
     });
    
 }
@@ -63,8 +63,8 @@ search_button.addEventListener("click", async () => {
     const query = search_input.value.trim();
     if(query !== ""){
         try{
-            const articles = await getQuery(query);
-            displayNews(articles);
+            const gifResults = await getQuery(query);
+            displayGifs(gifResults);
         }
         catch(error){
             console.log("Search not found", error);
@@ -74,10 +74,10 @@ search_button.addEventListener("click", async () => {
 
 (async () => {
     try{
-        const articles = await renderHeadlines();
-        displayNews(articles);
+        const gifResults = await renderHomepage();
+        displayGifs(gifResults);
     }
     catch(error){
-        console.error("Error rendering news", error);
+        console.error("Error rendering gifs", error);
     }
 })();
